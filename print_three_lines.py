04bb793b0366341ca91f2bdf806cae788ad59aa0
Jaeryn Aboagye <Jaeryn.aboagye@Icloud.com>
2024-08-13 21:04:40 +0000

    assignment 3

---
+++ print_three_lines.py
@@ -0,0 +1,3 @@
+print("Python is a versatile programming language.")
+print("It is widely used in web development, data science, and automation.")
+print("Learning Python opens up many career opportunities.")
