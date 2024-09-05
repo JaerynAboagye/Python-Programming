433c343e0ce13cf7dbd1ba0ef7eb3645ada06eba
Jaeryn Aboagye <Jaeryn.aboagye@Icloud.com>
2024-09-05 20:08:11 +0000

    assignment 46

---
+++ edgyblackhedgehogwhohatessonic.py
@@ -0,0 +1,2 @@
+str = "Python is an interpreted, interactive, object-oriented programming language that combined remarkable power with very clear syntax"
+print(str[38:66] + str[106:112] + str[0:6])
